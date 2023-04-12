6f6749b5753576c47f3d43b383a6811dd8f97b60
jovialcore <chidideveloper@gmail.com>
2023-04-12 16:41:43 +0100

    controller logic for FE done

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\BookResource;
+use App\Models\Book;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
+
+
+class BookController extends Controller
+{
+    public function index(Request $req)
+    {
+        $books = Book::all();
+        return view("Book.index", compact('books'));
+    }
+
+
+
+    public function edit(Request $req, $id)
+    {
+        $book = Book::findOrFail($id);
+
+        $req->validate($req->all(), [
+
+            'name' => 'required|string',
+            'isbn' => 'required|integer',
+            'authors' => 'required|string',
+            'number_of_pages' => 'required',
+            'publisher' => 'required|string',
+            'release_date' => 'required',
+            'country' => 'required|string'
+        ]);
+
+        return redirect()->with('msg', 'Books updated successfully');
+    }
+
+
+    public function delete($id)
+    {
+        Book::destroy($id);
+
+        return redirect()->with('msg', 'Book deleted successfully');
+    }
+}
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,8 +17,11 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
 </head>
+
 <body>
     <div id="app">
 
@@ -26,4 +30,5 @@
         </main>
     </div>
 </body>
+
 </html>
